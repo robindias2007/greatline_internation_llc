cbebd6e0178feca3bb0ae508f326dcd973948254
Robin Dias <robindias2007@gmail.com>
2018-08-05 02:25:12 +0530

    added hotels

---
+++ app/controllers/hotels_controller.rb
@@ -0,0 +1,7 @@
+class HotelsController < ApplicationController
+  #before_action :authenticate_support, :only => [:index]
+
+	def index
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get '/excursions' => 'excursions#index'
   get '/galleries' => 'galleries#index'
   get '/tours' => 'tours#index'
+  get '/hotels' => 'hotels#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
